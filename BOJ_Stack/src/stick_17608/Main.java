904150ef13d801c56b698a47a310c7717bd47b76
이지희 <som2plestory@naver.com>
2024-03-21 01:09:06 +0900

    백준 자바 - 막대기

---
+++ BOJ_Stack/src/stick_17608/Main.java
@@ -0,0 +1,37 @@
+package stick_17608;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 막대기의 길이 list
+		List<Integer> list = new ArrayList<>();
+		// 막대기의 개수
+		int n = Integer.parseInt(br.readLine());
+		// n개 막대기의 길이 (왼쪽부터 list.[0])
+		for(int i=0; i<n; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		// 가장 오른쪽(list의 마지막) 막대기부터 지금까지 막대 길이의 최대값
+		int max = 0;
+		// 지금까지 보이는 막대 개수
+		int count = 0;
+		
+		for(int i=n-1; i>=0; i--) {
+			// 지금까지 막대길이보다 긴 막대면 보일 것이다(count++)
+			if(list.get(i) > max) {
+				count++;
+				max = list.get(i);
+			}
+		}
+		
+		System.out.println(count);
+		br.close();
+	}
+}
