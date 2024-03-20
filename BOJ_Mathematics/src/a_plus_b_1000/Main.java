1df5a75f93fbae9aa80a2a7bf92c4cd91519fa63
이지희 <som2plestory@naver.com>
2024-03-20 21:59:36 +0900

    A 더하기 B
    수학 구현 사칙현산

---
+++ BOJ_Mathematics/src/a_plus_b_1000/Main.java
@@ -0,0 +1,11 @@
+package a_plus_b_1000;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()+sc.nextInt());
+		sc.close();
+	}
+}
