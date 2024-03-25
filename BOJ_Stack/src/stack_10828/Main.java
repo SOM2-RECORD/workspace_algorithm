76e110c65f6bc147f05a011b24950d7d52f93d78
이지희 <som2plestory@naver.com>
2024-03-25 18:51:38 +0900

    240325-1

---
+++ BOJ_Stack/src/stack_10828/Main.java
@@ -0,0 +1,71 @@
+package stack_10828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+	
+	static List<Integer> stack;
+	
+	static void push(int x) {
+		stack.add(x);
+	}
+	
+	static int pop() {
+		if(stack.isEmpty()) {
+			return -1;
+		}
+		int n = stack.get(stack.size()-1);
+		stack.remove(stack.size()-1);
+		return n;
+	}
+	
+	static int size() {
+		return stack.size();
+	}
+	
+	static int empty() {
+		if(stack.isEmpty()) {
+			return 1;
+		}
+		return 0;
+	}
+	
+	static int top() {
+		if(stack.isEmpty()) {
+			return -1;
+		}
+		return stack.get(stack.size()-1);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		stack = new LinkedList<>();
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<n; i++) {
+			String[] ask = br.readLine().split(" ");
+			switch(ask[0]) {
+				case "push":
+					push(Integer.parseInt(ask[1]));
+					break;
+				case "pop":
+					System.out.println(pop());;
+					break;
+				case "size":
+					System.out.println(size());
+					break;
+				case "empty":
+					System.out.println(empty());
+					break;
+				case "top":
+					System.out.println(top());
+					break;
+			}
+		}
+		br.close();
+	}
+}
