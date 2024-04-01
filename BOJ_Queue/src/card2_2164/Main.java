29aec34a538102af5f195310a440c1359fbb72ed
이지희 <som2plestory@naver.com>
2024-04-02 00:42:44 +0900

    2164 카드2

---
+++ BOJ_Queue/src/card2_2164/Main.java
@@ -0,0 +1,30 @@
+package card2_2164;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=n; i++) {
+        	queue.add(i);
+        }
+        
+        while(queue.size()>1) {
+        	queue.poll();
+        	if(queue.size() == 1) {
+        		break;
+        	}
+        	int k = queue.poll();
+        	queue.add(k);
+        }
+        
+        System.out.println(queue.peek());
+        br.close();
+    }
+}
