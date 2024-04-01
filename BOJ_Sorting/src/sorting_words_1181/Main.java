413d6ec6ee1bb4565b9a970e8eb5d75a52756db8
이지희 <som2plestory@naver.com>
2024-04-02 01:28:00 +0900

    단어 정렬 1181 -list

---
+++ BOJ_Sorting/src/sorting_words_1181/Main.java
@@ -0,0 +1,42 @@
+package sorting_words_1181;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+        	String str = br.readLine();
+        	if(!list.contains(str)) {
+        		list.add(str);
+        	}
+        }
+        
+        Collections.sort(list, new Comparator<String>() {
+			@Override
+			public int compare(String a, String b) {
+				if(a.length() != b.length()) {
+					return Integer.compare(a.length(), b.length());
+				}else {
+					return a.compareTo(b);
+				}
+			}
+		});
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<list.size(); i++) {
+        	sb.append(list.get(i)).append("\n");
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+}
