291918c42978901369d9138737295d183d463a37
이지희 <som2plestory@naver.com>
2024-04-23 13:21:56 +0900

    7569 토마토

--- BOJ_GraphTraversal/src/tomato_7569/Main.java
+++ BOJ_GraphTraversal/src/tomato_7569/Main.java
@@ -3,41 +3,89 @@ package tomato_7569;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][][] tomato;
-	static boolean[][][] visited;
-	static int date = 0;
+	static class Tomato{
+		int x, y, z, d;
+		Tomato(int x, int y, int z, int d){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.d = d;
+		}
+	}
 	
-	static void BFS(int m, int n, int h) {
-		
+	static int M, N, H;
+	static int[][][] box;
+	static int[] dx = {-1, 1, 0, 0, 0, 0}; 
+	static int[] dy = {0, 0, 1, -1, 0, 0}; 
+	static int[] dz = {0, 0, 0, 0, 1, -1}; 
+	static int days = 0;
+	static int not_exist = 0;
+	static int ripe = 0;
+	static Queue<Tomato> queue = new LinkedList<>();
+	
+	static void BFS() {
+		while(!queue.isEmpty()) {
+			Tomato tomato = queue.poll();
+			
+			for(int i=0; i<6; i++) {
+				int x = tomato.x + dx[i];
+				int y = tomato.y + dy[i];
+				int z = tomato.z + dz[i];
+				
+				if(x<0 || x>=M || y<0 || y>=N || z<0 || z>=H || box[x][y][z] == -1 || box[x][y][z] == 1) continue;
+				
+				box[x][y][z] = 1;
+				ripe ++;
+				queue.add(new Tomato(x, y, z, tomato.d + 1));
+				days = tomato.d + 1;
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int M = Integer.parseInt(st.nextToken()); //상자 가로 길이
-		int N = Integer.parseInt(st.nextToken()); //상자 세로 길이
-		int H = Integer.parseInt(st.nextToken()); //상자 높이
+		M = Integer.parseInt(st.nextToken()); //상자 가로 길이
+		N = Integer.parseInt(st.nextToken()); //상자 세로 길이
+		H = Integer.parseInt(st.nextToken()); //상자 높이
 		
-		tomato = new int[M][N][H];
-		visited = new boolean[M][N][H];
+		box = new int[M][N][H];
 		
 		for(int h=0; h<H; h++) {
-			for(int m=0; m<M; m++) {
+			for(int n=0; n<N; n++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				for(int n=0; n<N; n++) {
-					tomato[m][n][h] = Integer.parseInt(st.nextToken());
+				for(int m=0; m<M; m++) {
+					box[m][n][h] = Integer.parseInt(st.nextToken());
+					if(box[m][n][h] == 1) {
+						queue.add(new Tomato(m, n, h, 0));
+						ripe++;
+						continue;
+					}
+					if(box[m][n][h] == -1) not_exist++;
 				}
 			}
 		}
 		
+		int count = M*N*H-not_exist;
+		if(ripe == count) {
+			System.out.println(0);
+			return;
+		}
 		
+		BFS();
 		
+		if(ripe < count) {
+			System.out.println(-1);
+			return;
+		}
 		
-		
+		System.out.println(days);
 		br.close();
 	}
 }
